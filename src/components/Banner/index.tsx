2db40278f83c5e68e667151b711465172aad3ca5
jeffersoncharlles <contato@jefferdeveloper.com>
2022-01-21 15:53:02 -0300

    chore: banner commit

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+
+export const Banner = () => {
+
+    return (
+        <div className={styles.banner}>
+            <h1>Twitter dev</h1>
+        </div>
+    );
+}
---
+++ src/components/InfoUser/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+
+export const InfoUser = () => {
+
+    return (
+        <section className={styles.container}>
+
+        </section>
+    );
+}
